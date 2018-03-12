d83dfd0418b89be30fbddb5f7d987a87c9f22e06
Mansi Gupta <imansigupta29@icloud.com>
2018-03-12 19:14:11 -0400

    Initial Commit

---
+++ Photorama/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Photorama
+//
+//  Created by Mansi Gupta on 2018-03-12.
+//  Copyright © 2018 Mansi Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
